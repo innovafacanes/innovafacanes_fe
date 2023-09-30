0a2b4148c68d6c05bf9b02f3223393eb7f7667bf
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-30 17:41:38 +0200

    Change navBar words depending on language

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,33 @@ import LanguageContext from "@/store/language/context/LanguageContext";
 import styles from "@/styles/Navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function Navbar() {
   const [mobileMenuState, setMobileMenuState] = useState(false);
-  const { setChosenLanguage } = useContext(LanguageContext);
+  const [navWords, setNavWords] = useState([
+    "Serveis",
+    "Projectes",
+    "Nosaltres",
+    "Contacte",
+  ]);
+  const { setChosenLanguage, language } = useContext(LanguageContext);
+
+  useEffect(() => {
+    navWord(language);
+  }, [language]);
+
+  const navWord = (language) => {
+    if (language === "ca") {
+      setNavWords(["Serveis", "Projectes", "Nosaltres", "Contacte"]);
+    }
+    if (language === "es") {
+      setNavWords(["Servicios", "Proyectos", "Nosotros", "Contacto"]);
+    }
+    if (language === "en") {
+      setNavWords(["Services", "Projects", "About us", "Contact"]);
+    }
+  };
 
   const handleMobileMenu = () => {
     setMobileMenuState((mobileMenuState) => !mobileMenuState);
@@ -31,16 +53,16 @@ export default function Navbar() {
         </div>
         <div className={styles.navTagsWrapper}>
           <Link href="/serveis" rel="noopener noreferrer">
-            <div className={styles.navItems}>Serveis</div>
+            <div className={styles.navItems}>{navWords[0]}</div>
           </Link>
           <Link href="/projectes" rel="noopener noreferrer">
-            <div className={styles.navItems}>Projectes</div>
+            <div className={styles.navItems}>{navWords[1]}</div>
           </Link>
           <Link href="/nosaltres" rel="noopener noreferrer">
-            <div className={styles.navItems}>Nosaltres</div>
+            <div className={styles.navItems}>{navWords[2]}</div>
           </Link>
           <Link href="/contacte" rel="noopener noreferrer">
-            <div className={styles.navItems}>Contacte</div>
+            <div className={styles.navItems}>{navWords[3]}</div>
           </Link>
           <div className={styles.langWrapper}>
             <button onClick={() => setChosenLanguage("ca")}>

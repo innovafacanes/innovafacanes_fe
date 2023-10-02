d7639ad73069edf39e995963b7c9abedc17873d0
eudaldmoya <eudald.moya.8@gmail.com>
2023-10-02 17:39:18 +0200

    Change language in Projectes page

--- src/pages/projectes.js
+++ src/pages/projectes.js
@@ -1,107 +1,135 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
-import { fetchProjectes } from "./api/fetching";
+import useStrapiApi, { fetchProjectes } from "./api/fetching";
 import Footer from "../../components/Footer";
 import styles from "@/styles/Projectes.module.css";
 import Image from "next/image";
 import Popup from "reactjs-popup";
+import LanguageContext from "@/store/language/context/LanguageContext";
 
-export default function Projectes({ projects }) {
+export default function Projectes() {
   const [open, setOpen] = useState(false);
   const [activeProject, setActiveProject] = useState(null);
   const closeModal = () => setOpen(false);
+  const { fetchProjectes } = useStrapiApi();
+  const { language } = useContext(LanguageContext);
+  const [info, setInfo] = useState([]);
+  const [title, setTitle] = useState(["Projectes"]);
+
+  useEffect(() => {
+    (async () => {
+      const {
+        props: { projects },
+      } = await fetchProjectes(language);
+
+      setInfo(projects);
+    })();
+  }, [fetchProjectes, language]);
+
+  useEffect(() => {
+    changeTitle(language);
+  }, [language]);
+
+  const changeTitle = (language) => {
+    if (language === "ca") {
+      setTitle(["Projectes"]);
+    }
+    if (language === "es") {
+      setTitle(["Proyectos"]);
+    }
+    if (language === "en") {
+      setTitle(["Projects"]);
+    }
+  };
 
   return (
     <>
       <Navbar />
       <div className={styles.mainProjectes}>
-        <h1>PROJECTES</h1>
-        <div className={styles.projectsWrapper}>
-          {projects.map((project, index) => (
-            <div key={index}>
-              <div
-                className={styles.projectWrapper}
-                onClick={() => {
-                  setOpen((o) => !o);
-                  setActiveProject(index);
-                }}
-              >
-                <Image
-                  className={styles.projectImg}
-                  src={`${project.images.data[0].attributes.url}`}
-                  alt={project.images.data[0].attributes.alternativeText}
-                  width={450}
-                  height={450}
-                />
-                <h2 className={styles.projectTitle}>{project.title}</h2>
-              </div>
+        <h1>{title}</h1>
+        {info && (
+          <div className={styles.projectsWrapper}>
+            {info.map((project, index) => (
+              <div key={index}>
+                <div
+                  className={styles.projectWrapper}
+                  onClick={() => {
+                    setOpen((o) => !o);
+                    setActiveProject(index);
+                  }}
+                >
+                  <Image
+                    className={styles.projectImg}
+                    src={`${project.images.data[0].attributes.url}`}
+                    alt={project.images.data[0].attributes.alternativeText}
+                    width={450}
+                    height={450}
+                  />
+                  <h2 className={styles.projectTitle}>{project.title}</h2>
+                </div>
 
-              <Popup
-                key={index}
-                open={activeProject === index ? open : false}
-                className={styles.popupContent}
-                closeOnDocumentClick
-                onClose={closeModal}
-                overlayStyle={{ background: "rgba(0,0,0,0.5)" }}
-                contentStyle={{
-                  margin: "auto",
-                  position: "relative",
-                  width: "80%",
-                  height: "fit-content",
-                  background: "white",
-                  borderRadius: "0rem",
-                  boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  padding: "1rem",
-                }}
-              >
-                <div className={styles.popupContentWrapper}>
-                <div className={styles.detailImgCarousel}>
-                  <div className={styles.carouselImgWrapper}>
-                    {project.images.data.map((img, index) => (
-                      <Image
-                        key={index}
-                        className={styles.projectDetailImg}
-                        src={`${img.attributes.url}`}
-                        alt={img.attributes.alternativeText}
-                        width={600}
-                        height={450}
-                      />
-                    ))}
+                <Popup
+                  key={index}
+                  open={activeProject === index ? open : false}
+                  className={styles.popupContent}
+                  closeOnDocumentClick
+                  onClose={closeModal}
+                  overlayStyle={{ background: "rgba(0,0,0,0.5)" }}
+                  contentStyle={{
+                    margin: "auto",
+                    position: "relative",
+                    width: "80%",
+                    maxWidth: "1200px",
+                    height: "fit-content",
+                    background: "white",
+                    borderRadius: "0rem",
+                    boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    padding: "1rem",
+                  }}
+                >
+                  <div className={styles.popupContentWrapper}>
+                    <div className={styles.detailImgCarousel}>
+                      <div className={styles.carouselImgWrapper}>
+                        {project.images.data.map((img, index) => (
+                          <Image
+                            key={index}
+                            className={styles.projectDetailImg}
+                            src={`${img.attributes.url}`}
+                            alt={img.attributes.alternativeText}
+                            width={600}
+                            height={450}
+                          />
+                        ))}
+                      </div>
+                      <div class={styles.controls}>
+                        <button id="prevBtn" className={styles.prevBtn}>
+                          Previous
+                        </button>
+                        <button id="nextBtn" className={styles.nextBtn}>
+                          Next
+                        </button>
+                      </div>
+                    </div>
+                    <div className={styles.detailText}>
+                      <h3 className={styles.detailTitle}>{project.title}</h3>
+                      <p className={styles.detailDescription}>
+                        {project.description}
+                      </p>
+                      <p className={styles.detailDate}>{project.date}</p>
+                    </div>
+                    <a className={styles.close} onClick={closeModal}>
+                      &times;
+                    </a>
                   </div>
-                  <div class={styles.controls}>
-                    <button id="prevBtn" className={styles.prevBtn}>
-                      Previous
-                    </button>
-                    <button id="nextBtn" className={styles.nextBtn}>
-                      Next
-                    </button>
-                  </div>
-                </div>
-                <div className={styles.detailText}>
-                  <h3 className={styles.detailTitle}>{project.title}</h3>
-                  <p className={styles.detailDescription}>
-                    {project.description}
-                  </p>
-                  <p className={styles.detailDate}>
-                    {project.date}
-                  </p>
-                </div>
-                <a className={styles.close} onClick={closeModal}>
-                  &times;
-                </a>
-                </div>
-              </Popup>
-            </div>
-          ))}
-        </div>
+                </Popup>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </>
   );
-}
-
-export async function getStaticProps() {
-  return fetchProjectes('ca');
 }
